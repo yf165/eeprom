9107cb4adab721933baea135c24abb2086350ef2
yf165 <35261313+yf165@users.noreply.github.com>
2018-02-02 12:53:26 +0800

    Rev 5.0
    cp /test

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ all:
 	arm-none-linux-gnueabi-gcc $(obj).c eeprom.c -o $(obj).out
 
 cp:
-	cp $(obj).out /root/porting_x210/rootfs/rootfs/test/driver_test -f
+	cp $(obj).out /root/porting_x210/rootfs/rootfs/test/ -f
 .PHONY: clean	
 clean:
 	rm -rf *.out *.o
--- main.c
+++ main.c
@@ -1,21 +1,27 @@
 #include "eeprom.h"
 #include <unistd.h>
 #include <string.h>
-
+#include <stdlib.h>
+#include <stdio.h>
 #define DEVICE "/dev/i2c-0"
 #define DEVADDR 0x50
+
 
 int main(int argc,char **argv)
 {
 	unsigned char writeBuf[256] ={0};
-	unsigned char readBuf[256] ={0};	
+	unsigned char readBuf[256] = {0};	
+
 	int fd = openEep(DEVICE);
 	if (argc == 2){
 		memset(writeBuf,0,sizeof(writeBuf));
 		memcpy(writeBuf,*(argv+1),strlen(*(argv+1)));
 		writeEep(fd,DEVADDR,128,writeBuf, strlen(writeBuf));		
 	}
+
 	readEep(fd,DEVADDR,128,64,readBuf);
 	printBuf(readBuf,128,64);
+
 	closeEep(fd);
+	return 0;
 }
